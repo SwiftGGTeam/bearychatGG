d0214115e94e0bb15a0276e082c3c5ce6070607e
梁杰 <lj925184928@gmail.com>
2015-09-06 14:52:38 +0800

    add authorization status tag

--- main.py
+++ main.py
@@ -49,31 +49,31 @@ authorized = [
     {
         "store_file_name": "natashatherobot_profile",
         "url": "http://natashatherobot.com/feed/",
-        "msg_title": "Natasha The Robot",
+        "msg_title": "[已授权] Natasha The Robot",
         "typeis": "rss"
     },
     {
         "store_file_name": "swiftandpainless_profile",
         "url": "http://swiftandpainless.com/feed/",
-        "msg_title": "Swift and Painless",
+        "msg_title": "[已授权] Swift and Painless",
         "typeis": "rss"
     },
     {
         "store_file_name": "nsblog_profile",
         "url": "https://www.mikeash.com/pyblog/rss.py",
-        "msg_title": "NSBlog(mikeash.com)",
+        "msg_title": "[已授权] NSBlog(mikeash.com)",
         "typeis": "rss"
     },
     {
         "store_file_name": "ericasadun_profile",
         "url": "http://ericasadun.com/feed/",
-        "msg_title": "Erica Sadun",
+        "msg_title": "[已授权] Erica Sadun",
         "typeis": "rss"
     },
     {
         "store_file_name": "ioscreator_profile",
         "url": "http://www.ioscreator.com/swift/",
-        "msg_title": "IOSCREATOR",
+        "msg_title": "[已授权] IOSCREATOR",
         "anchors": ["sqs-block-content"],
         "get_full_data": ic_get_full_data,
         "typeis": "html"
@@ -81,13 +81,13 @@ authorized = [
     {
         "store_file_name": "appventure_profile",
         "url": "http://appventure.me/rss-feed",
-        "msg_title": "APPVENTURE",
+        "msg_title": "[已授权] APPVENTURE",
         "typeis": "rss"
     },
     {
         "store_file_name": "szulctomasz_profile",
         "url": "http://szulctomasz.com/",
-        "msg_title": "Tomasz Szulc",
+        "msg_title": "[已授权] Tomasz Szulc",
         "anchors": ["post"],
         "get_full_data": ts_get_full_data,
         "typeis": "html"
@@ -95,7 +95,7 @@ authorized = [
     {
         "store_file_name": "thomashanning_profile",
         "url": "http://www.thomashanning.com/category/swift/",
-        "msg_title": "THOMAS HANNING",
+        "msg_title": "[已授权] THOMAS HANNING",
         "anchors": ["entry-title"],
         "get_full_data": th_get_full_data,
         "typeis": "html"
@@ -103,14 +103,14 @@ authorized = [
     {
         "store_file_name": "ole_profile",
         "url": "http://oleb.net/blog/atom.xml",
-        "msg_title": "Ole Begemann",
+        "msg_title": "[已授权] Ole Begemann",
         "cus_get_full_data": o_get_full_data,
         "typeis": "rss"
     },
     {
         "store_file_name": "raj_profile",
         "url": "http://rajkandathi.com/category/ios/",
-        "msg_title": "Raj Kandathi",
+        "msg_title": "[已授权] Raj Kandathi",
         "anchors": ["entry-title"],
         "get_full_data": th_get_full_data,
         "typeis": "html"
@@ -118,19 +118,19 @@ authorized = [
     {
         "store_file_name": "chrunchy_profile",
         "url": "http://alisoftware.github.io/feed.xml",
-        "msg_title": "Crunchy Development",
+        "msg_title": "[已授权] Crunchy Development",
         "typeis": "rss"
     },
     {
         "store_file_name": "jameson_profile",
         "url": "http://jamesonquave.com/blog/feed/",
-        "msg_title": "JamesonQuave.com",
+        "msg_title": "[已授权] JamesonQuave.com",
         "typeis": "rss"
     },
     {
         "store_file_name": "jacob_profile",
         "url": "http://bandes-stor.ch/archive/",
-        "msg_title": "Jacob Bandes-Storch",
+        "msg_title": "[已授权] Jacob Bandes-Storch",
         "anchors": ["article"],
         "get_full_data": jbs_get_full_data,
         "typeis": "html"
@@ -157,7 +157,7 @@ unauthorized = [
     {
         "store_file_name": "raywenderlich_profile",
         "url": "http://www.raywenderlich.com/category/swift",
-        "msg_title": "Ray Wenderlich",
+        "msg_title": "[未授权] Ray Wenderlich",
         "anchors": ["articles", "tutorials"],
         "get_full_data": ray_get_full_data,
         "typeis": "html"
@@ -165,13 +165,13 @@ unauthorized = [
     {
         "store_file_name": "appleblog_profile",
         "url": "https://developer.apple.com/swift/blog/news.rss",
-        "msg_title": "苹果官方博客",
+        "msg_title": "[未授权] 苹果官方博客",
         "typeis": "rss"
     },
     {
         "store_file_name": "thoughtbot_profile",
         "url": "https://robots.thoughtbot.com/tags/swift",
-        "msg_title": "thoughtbot",
+        "msg_title": "[未授权] thoughtbot",
         "anchors": ["mini-post-title"],
         "get_full_data": tb_get_full_data,
         "typeis": "html"
